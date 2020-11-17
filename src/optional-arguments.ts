09ddca31eb58aa730d29f2b6e0ba3eff3705aff6
y4shiro <y4shironao@gmail.com>
2020-11-17 16:31:33 +0900

    add optional-arguments.ts

---
+++ src/optional-arguments.ts
@@ -0,0 +1,20 @@
+export {};
+
+let bmi: (height: number, weight: number, printable?: boolean) => number = (
+  height: number,
+  weight: number,
+  printable?: boolean
+): number => {
+  const bmi: number = weight / (height * height);
+  if (printable) {
+    console.log({ bmi });
+  }
+  return bmi;
+};
+
+bmi(1.78, 86, true);
+
+// bmi(身長、体重、console.log で出力するかどうか)
+// bmi(1.78, 86, true);
+// bmi(1.78, 86, false);
+// bmi(1.78, 86);
